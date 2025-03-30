24c2f455b4337c4daf16151ac1ea1cc8f1fd0443
Alex Munoz <145365915+qcklex@users.noreply.github.com>
2025-03-30 06:35:52 +0100

    unicafe 1.11  updatedtable

--- part1/introdemo/src/components/Statistics.jsx
+++ part1/introdemo/src/components/Statistics.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
-import { useState } from 'react'
+import './statistics.css'
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      {props.text} 
+      
+      <div className='value'>{props.value}</div>
+      
+    </div>
+  )}
 
  const Statistics = (props) => {
   const all = (props.good + props.neutral + props.bad)
   const average = all / 3
   const positive = (props.good / all) * 100
 
-  return (
+  return(
     <div>
      <h1>statistics</h1>
      {all > 0 ?
-     <div>
-      <div>good {props.good}</div>
-      <div>neutral {props.neutral}</div>
-      <div>bad {props.bad}</div>
-      <div>all {all}</div>
-      <div>average {average}</div>
-      <div>positive {positive}</div>
+     <div className='statistics'>
+      <StatisticLine text="good" value={props.good} />
+      <StatisticLine text="neutral" value={props.neutral} />
+      <StatisticLine text="bad" value={props.bad} />
+      <div>all<div className='value'>{all}</div></div>
+      <div>average <div className='value'>{average}</div></div>
+      <div>positive <div className='value'>{positive}</div></div>
       </div> : <div> No feedback given</div>}
     </div>
-      
-  )
-
+      )
 }
 
 export default Statistics
