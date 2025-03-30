9977ee2c3852f33bd05324f7f4c29955bd624acd
Alex Munoz <145365915+qcklex@users.noreply.github.com>
2025-03-30 06:40:59 +0100

    anecdotes exercise12

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,47 +1,33 @@
 import { useState } from 'react'
-import React from 'react'
-import Statistics from '../../unicafe/components/Statistics'
-
-const Button = (props) => {
-
-  return (
-    <button onClick={props.onClick}>{props.text}</button>
-  )
-}
-
-
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  
-  function handleSetGood(){
-    setGood(good + 1)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+function handleSetSelected(){
+    setSelected(Math.floor(Math.random() * anecdotes.length))
   }
-  function handleSetNeutral(){
-    setNeutral(neutral + 1)
-  }
-  function handleSetBad(){
-    setBad(bad + 1)
+  const handleClick = () => {
+    handleSetSelected()
   }
 
-    return (
-      <>
-        <div>
-          <h1>give feedback</h1>
-          <Button onClick={handleSetGood} text="good"/>
-          <Button onClick={handleSetNeutral} text="neutral"/>
-          <Button onClick={handleSetBad} text="bad"/>
-        </div>
-        <Statistics good={good} neutral={neutral} bad={bad}/>
-      </>
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br />
+    <button onClick={handleClick}>next anecdote</button>
+    </div>
+
   )
 }
 
-
-
-
 export default App
-  
