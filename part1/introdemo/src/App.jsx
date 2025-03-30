0902eda6006a7305f812663b3bba5b07b0f054d1
Alex Munoz <145365915+qcklex@users.noreply.github.com>
2025-03-30 07:52:18 +0100

    anecdotes exercise14

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,30 +1,61 @@
 import { useState } from 'react'
+import React from 'react'
+import './App.css'
 
 const App = () => {
-  const anecdotes = [
+  const anecdotes = {
+    quotes: [
     'If it hurts, do it more often.',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Any fool can write code that a computer can understand. Good programmers write ¡code that humans can understand.',
     'Premature optimization is the root of all evil.',
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
     'The only way to go fast, is to go well.'
-  ]
+  ],
+
+}
+
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0, 0])
+  const [maxVotesIndex, setMaxVotesIndex] = useState(0)
+
 function handleSetSelected(){
-    setSelected(Math.floor(Math.random() * anecdotes.length))
+    setSelected(Math.floor(Math.random() * anecdotes.quotes.length))
   }
   const handleClick = () => {
     handleSetSelected()
   }
+  const handleVote = () => {
+    let newVotes = [...votes] 
+    newVotes[selected] += 1
+    setVotes(newVotes)
+    setMaxVotesIndex(votes.indexOf(Math.max(...votes)))
+  }
 
   return (
-    <div>
-      {anecdotes[selected]}
+    <div className="anecdotes">
+      <div className='quote'>
+        {anecdotes.quotes[selected]}
+      </div>
       <br />
+      <div className='votes'>
+        has {votes[selected]} votes
+      </div>
     <button onClick={handleClick}>next anecdote</button>
+    <button onClick={handleVote}>vote</button>
+    <h3 className='quote'>
+      Anecdote with most votes 
+      </h3>
+      <div className='quote'>
+        {anecdotes.quotes[maxVotesIndex]}
+      </div>
+      <br />
+      <div className='max-votes'>
+        has {votes[maxVotesIndex]} votes
+      </div>
     </div>
 
   )
