90cf6e022aed07cfa668d5147b08f1bf7a5208fa
Alex Munoz <145365915+qcklex@users.noreply.github.com>
2025-04-12 00:46:57 +0100

    part2c phonebook2.11

--- doing/README.md
+++
@@ -1,2 +0,0 @@
-# FullStackOpenExercises
-Repository for all my exercises of the Full Stack Open course by the Helsinki University.
--- part2/phonebook/main.jsx
+++ part2/phonebook/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-
+import ReactDOM, { createRoot } from 'react-dom/client'
 import App from './App'
+import axios from 'axios'
 
-  
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-  )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
